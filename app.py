fd56d74ca61c4569e8b27a2aed2fca3f09e9def0
Wojciech Terepeta <wojciech.terepeta@gmail.com>
2015-03-20 15:22:03 +0100

    First try to do whatever.

--- app.py
+++ app.py
@@ -8,21 +8,22 @@ from bottle_ac import create_addon_app
 
 log = logging.getLogger(__name__)
 app = create_addon_app(__name__,
-                       plugin_key="hc-standup",
-                       addon_name="HC Standup",
+                       plugin_key="gi-standup",
+                       addon_name="GI Standup",
                        from_name="Standup",
                        base_url="http://192.168.33.1:8080")
 
-app.config['MONGO_URL'] = os.environ.get("MONGO_URL", None)
+#app.config['MONGO_URL'] = os.environ.get("MONGO_URL", None)
 app.config['REDIS_URL'] = os.environ.get("REDISTOGO_URL", None)
 
+standup_db = {}
 
 def init():
     @asyncio.coroutine
     def _send_welcome(event):
         client = event['client']
         yield from client.send_notification(app.addon,
-            text="HC Standup was added to this room. Type '/standup I did *this*' to get started (yes, "
+            text="GI Standup was added to this room. Type '/standup I did *this*' to get started (yes, "
                  "you can use Markdown).")
 
     app.addon.register_event('install', _send_welcome)
@@ -41,10 +42,10 @@ def capabilities(request, response):
         },
         "key": app.config.get("PLUGIN_KEY"),
         "name": app.config.get("ADDON_NAME"),
-        "description": "HipChat connect add-on that supports async standups",
+        "description": "HipChat connect add-on that supports async standups (Gank Incoming fork)",
         "vendor": {
-            "name": "Atlassian Labs",
-            "url": "https://atlassian.com"
+            "name": "Gank Incoming",
+            "url": ""
         },
         "capabilities": {
             "installable": {
@@ -92,28 +93,20 @@ def standup(request, response):
 
 @asyncio.coroutine
 def record_status(addon, client, from_user, status):
-    spec, statuses = yield from find_statuses(addon, client)
-
     user_mention = from_user['mention_name']
-    statuses[user_mention] = {
+
+    standup_db[user_mention] = {
         "user": from_user,
         "message": status,
         "date": datetime.utcnow()
     }
-
-    data = dict(spec)
-    data['users'] = statuses
-
-    yield from standup_db(addon).update(spec, data, upsert=True)
 
     yield from client.send_notification(addon, text="Status recorded.  Type '/standup' to see the full report.")
 
 
 @asyncio.coroutine
 def display_one_status(addon, client, mention_name):
-    spec, statuses = yield from find_statuses(addon, client)
-
-    status = statuses.get(mention_name)
+    status = standup_db[mention_name]
     if status:
         yield from client.send_notification(addon, html=render_status(status))
     else:
@@ -123,9 +116,7 @@ def display_one_status(addon, client, mention_name):
 
 @asyncio.coroutine
 def display_all_statuses(addon, client):
-    spec, statuses = yield from find_statuses(addon, client)
-
-    if statuses:
+    if standup_db:
         yield from client.send_notification(addon, html=render_all_statuses(statuses))
     else:
         yield from client.send_notification(addon, text="No status found. "
@@ -176,10 +167,6 @@ def status_spec(client):
         "group_id": client.group_id,
         "capabilities_url": client.capabilities_url
     }
-
-
-def standup_db(addon):
-    return addon.mongo_db.default_database['standup']
 
 
 if __name__ == "__main__":
