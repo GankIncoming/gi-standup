56cd9adf609e1383c7453123bc152684cb7e02d3
Wojciech Terepeta <wojciech.terepeta@gmail.com>
2015-05-01 01:42:07 +0200

    Status expiration works on paper.

--- app.py
+++ app.py
@@ -6,17 +6,19 @@ import arrow
 import markdown
 from bottle_ac import create_addon_app
 
+parameter_prefix = "--"
+parameter_argument_separator = "="
+parameter_expiry_date = parameter_prefix + "expiry"
+
 log = logging.getLogger(__name__)
 app = create_addon_app(__name__,
                        plugin_key="gi-standup",
                        addon_name="GI Standup",
                        from_name="Standup",
-                       base_url="https://gi-standup.herokuapp.com")
+                       base_url="https://gi-standup-dev.herokuapp.com")
 
 app.config['MONGO_URL'] = os.environ.get("MONGO_URL", None)
 app.config['REDIS_URL'] = os.environ.get("REDISTOGO_URL", None)
-
-standup_db = {}
 
 def init():
     @asyncio.coroutine
@@ -27,7 +29,6 @@ def init():
                  "you can use Markdown).")
 
     app.addon.register_event('install', _send_welcome)
-
 
 app.add_hook('before_first_request', init)
 
@@ -70,6 +71,23 @@ def capabilities(request, response):
         }
     }
 
+def extract_status_parameters(status):
+    parameters = []
+
+    while len(status) > 0:
+        if status.startswith(parameter_prefix):
+            parameter, _, status = status.partition(' ')
+            parameters.append(parameter)
+            status = status.strip()
+        else:
+            break
+
+    return status, parameters
+
+
+@asyncio.coroutine
+def handle_parameters(addon, client, parameters):
+    pass
 
 @app.route('/standup', method='POST')
 @asyncio.coroutine
@@ -86,20 +104,69 @@ def standup(request, response):
     elif status.startswith("@") and ' ' not in status:
         yield from display_one_status(app.addon, client, mention_name=status.strip("@"))
     else:
-        yield from record_status(app.addon, client, from_user, status)
+        status, parameters = extract_status_parameters(status)
+
+        if len(status) > 0:
+            yield from record_status(app.addon, client, from_user, status, parameters)
+        else:
+            yield from handle_parameters(app.addon, client, parameters)
 
     response.status = 204
 
 
 @asyncio.coroutine
-def record_status(addon, client, from_user, status):
-    spec, statuses = yield from find_statuses(addon, client)
+def handle_expiry_date_parameter(parameters):
+    argument = ""
 
+    for parameter in parameters:
+        if parameter.startswith("--expiry"):
+        #if parameter.startswith(parameter_expiry_date):
+            _, _, argument = parameter.partition("=")
+            break
+
+    if len(argument) <= 0:
+        print("Error: no argument")
+        return None
+
+    # Is the argument a date? E.g.: --expiry=10.03
+    if '.' in argument:
+        day, _, month = argument.partition('.')
+        date = datetime.utcnow().replace(day = int(day), month = int(month))
+
+        if date < datetime.utcnow():
+            return date.replace(year = date.year + 1)
+        return date
+
+    # Is the argument an interval? E.g.: --expiry=3d
+    try:
+        num = int(argument[:-1])
+    except:
+        print("Error: could not convert argument to integer")
+        return None
+
+    if argument.endswith('s'):
+        return datetime.utcnow() + timedelta(seconds = num)
+    elif argument.endswith('m'):
+        return datetime.utcnow() + timedelta(minutes = num)
+    elif argument.endswith('h'):
+        return datetime.utcnow() + timedelta(hours = num)
+    elif argument.endswith('d'):
+        return datetime.utcnow() + timedelta(days = num)
+
+    print("Error: invalid argument")
+    return None
+
+@asyncio.coroutine
+def record_status(addon, client, from_user, status, parameters):
+    spec, statuses = yield from find_statuses(addon, client)
     user_mention = from_user['mention_name']
+    expiry_date = handle_expiry_date_parameter(parameters)
+
     statuses[user_mention] = {
         "user": from_user,
         "message": status,
-        "date": datetime.utcnow()
+        "date": datetime.utcnow(),
+        "expiry": expiry_date
     }
 
     data = dict(spec)
@@ -142,13 +209,20 @@ def render_all_statuses(statuses):
 
 def render_status(status):
     msg_date = arrow.get(status['date'])
+    expiry_date = arrow.get(status["expiry"])
 
     message = status['message']
     html = markdown.markdown(message)
     html = html.replace("<p>", "")
     html = html.replace("</p>", "")
     name = status['user']['name']
-    return "<b>{name}</b>: {message} -- <i>{ago}</i>".format(name=name, message=html, ago=msg_date.humanize())
+
+    to_print = "<b>{name}</b>: {message} -- <i>{ago}, expiry: {expiry}</i>".format(name = name, message = html, ago = msg_date.humanize(),
+                                                                           expiry = expiry_date.humanize())
+
+    if status["expiry"] is not None and status["expiry"] < datetime.utcnow():
+        return "<s>" + to_print + "</s>"
+    return to_print
 
 
 @asyncio.coroutine
