d7018896b242361bf65535121f605dcda2f5f31b
Wojciech Terepeta <wojciech.terepeta@gmail.com>
2015-05-07 01:37:49 +0200

    Improved parameter support.

--- parameters.py
+++ parameters.py
@@ -9,6 +9,8 @@ short_desc_key = "short_description"
 long_desc_key = "long_description"
 
 argument_separator = "="
+prefix = "--"
+prefix_short = "-"
 
 class Parameter(object):
     def __init__(self, name, aliases, with_status, without_status, show_statuses, short_desc, long_desc):
@@ -19,14 +21,44 @@ class Parameter(object):
         self.show_statuses = show_statuses
         self.short_desc = short_desc
         self.long_desc = long_desc
+        self.short_help_str = self.name + ": " + self.short_desc
+        self.long_help_str = self.short_help_str + "\n" + self.long_desc + "\nAliases: " + ", ".join(self.aliases)
 
     def has_alias(self, alias):
         return alias in self.aliases
 
-def parse_json(filename):
-    parameters = {}
+class ParameterCollection(object):
+    def __init__(self):
+        self.alias_to_name = {}
+        self.parameters = {}
 
-    # TODO
+    def Get(self, alias):
+        return self.parameters[self.alias_to_name[alias]]
+
+    def Add(self, parameter):
+        for alias in parameter.aliases:
+            if alias in self.alias_to_name:
+                raise ValueError("Alias %s already exists in the dictionary!" % alias)
+
+        if parameter.name in self.parameters:
+            raise ValueError("Parameter %s already exists in the dictionary!" % parameter.name)
+
+        for alias in parameter.aliases:
+            self.alias_to_name[alias] = parameter.name
+
+        self.parameters[parameter.name] = parameter
+
+    def Has(self, alias):
+        return alias in self.alias_to_name
+
+def parse_json(filename):
+    parameters = ParameterCollection()
+    file = open(filename, 'r')
+    json_dict = json.load(file)
+    file.close()
+
+    for p_dict in json_dict["parameters"]:
+        parameters.Add(dict_to_parameter(p_dict))
 
     return parameters
 
